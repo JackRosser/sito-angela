71727e55a6826eb63254ca166b922ed06feaf248
Giacomo Rossettini <rossegiac@gmail.com>
2024-11-04 14:01:20 +0100

    funzione changetheme

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -9,16 +9,32 @@ export class ThemeService {
 
   constructor() {}
 
+navbarBg:string = "bg-orange-300"
+navbarButtonsBg: string = "bg-orange-500"
+navbarButtonsHover: string = "hover:bg-yellow-300 hover:text-orange-400"
+navbarMobileBg: string = "bg-orange-300"
+navbarMobileButtonHover: string = "hover:text-orange-400"
+navbarMobileIcon: string = "text-orange-500"
+mainBg: string = "bg-orange-100"
+
 private bhTheme = new BehaviorSubject<iTheme>({
-  navbarBg: 'fixed text-white py-2 pr-5 lg:px-5 w-full bg-orange-300 z-20',
-  navbarButtons: 'bg-orange-500 py-2 px-3 rounded-lg cursor-pointer hover:bg-yellow-300 hover:text-orange-400 transition-colors duration-300 hidden lg:block',
-  mobileBg: "flex flex-col items-end lg:hidden bg-orange-300 text-white px-5 py-5 rounded-bl-2xl",
-  mobileButton: "py-2 px-3 cursor-pointer hover:text-orange-400 transition-colors duration-300 custom-text-shadow",
-  spanIcon: "text-orange-500",
-  mainBg: 'bg-orange-100'
+  navbarBg: `fixed text-white py-2 pr-5 lg:px-5 w-full z-20 ${this.navbarBg}`,
+  navbarButtons: `py-2 px-3 rounded-lg cursor-pointer transition-colors duration-300 hidden lg:block ${this.navbarButtonsBg} ${this.navbarButtonsHover}`,
+  mobileBg: `flex flex-col items-end lg:hidden text-white px-5 py-5 rounded-bl-2xl ${this.navbarMobileBg}`,
+  mobileButton: `py-2 px-3 cursor-pointer transition-colors duration-300 custom-text-shadow ${this.navbarMobileButtonHover}`,
+  spanIcon: this.navbarMobileIcon,
+  mainBg: this.mainBg
 })
 theme$ = this.bhTheme.asObservable()
 
-
+changeTheme(navBg:string, navBtnBg:string, navBtnHoverBg:string, navBtnHoverText:string, navMobBg:string, navMobBtnHov:string, navMobIcon:string, main:string):void {
+this.navbarBg = navBg
+this.navbarButtonsBg = navBtnBg
+this.navbarButtonsHover = `hover:${navBtnHoverBg} hover:${navBtnHoverText}`
+this.navbarMobileBg = navMobBg
+this.navbarMobileButtonHover = navMobBtnHov
+this.navbarMobileIcon = navMobIcon
+this.mainBg = main
+}
 
 }
